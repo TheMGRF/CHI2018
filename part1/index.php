bebca43830c1cd7a07b0893bf2b58461dc114d4a
TheMGRF <thomas.grf@googlemail.com>
2020-10-26 15:17:41 +0000

    Master is master

--- part1/index.php
+++ part1/index.php
@@ -5,14 +5,12 @@ include_once 'classes/pages/RoutedWebPage.class.php';
 include_once 'classes/Router.class.php';
 
 $target = "";
-
-switch (isset($_GET["page"])) {
-    case "api":
-        $page = new Router("api");
-        break;
-    case "documentation":
-    default:
-        $page = new Router("documentation");
+if (isset($_GET["page"])) {
+    $target = $_GET["page"];
+} else {
+    $target = "documentation";
 }
+
+$page = new Router($target);
 echo $page->getPage();
 ?>
